b7b9027d3673520668170806bd59e103b48d3e39
Jeh3no <koxahiry@proton.me>
2025-06-20 18:47:29 +0200

    Damage number display update v2

--- addons/Targets/Scripts/ShootingRangeTargetScript.gd
+++ addons/Targets/Scripts/ShootingRangeTargetScript.gd
@@ -15,7 +15,7 @@ func _ready():
 func hitscanHit(damageVal : float, _hitscanDir : Vector3, _hitscanPos : Vector3):
 	health -= damageVal
 
-    #there is a tremendous issue with the display of damage number, that i don't understand, and i didn't manage to resolve it, so i've put an option to disable it, so that you don't see its errors (which don't affect gameplay  in any way, I might add)
+    #About the display of damage number, there are some tremendous errors with it, that i don't understand, and i didn't manage to resolve it, so i've put an option to disable it, so that you don't see theses errors (which don't affect gameplay in any way, i might add, but i preferred to add an option to not trigger them).
     if !isDisabled and canDisplayDamageNumber:
 	    DamageNumberScript.displayNumber(damageVal, damNumSpawnPoint.global_position, 110, DamageNumberScript.DamageType.NORMAL)
 	
